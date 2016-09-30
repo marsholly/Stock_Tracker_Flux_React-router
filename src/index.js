c8d24915bfce3a9fe04f8eb6c1627004785132f8
HONG ZHOU <holly_zhou@yahoo.com>
2016-09-30 00:40:25 -0700

    change back

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 
 import App from './components/App';
 import CompanyList from './components/CompanyList';
@@ -8,7 +8,7 @@ import StockView from './components/StockView';
 
 render(
   <Router history = {browserHistory}>
-    <Route path = '/Stock_Tracker_Flux_React-router/' component = {App}>
+    <Route path = '/' component = {App}>
       <Route path = 'stock' component = {StockView} />
     </Route>
     <Route path = 'companies' component = {CompanyList}/>
