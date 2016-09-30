5e518e9b66dd2cdf0a58217e351e5ade9b81893a
HONG ZHOU <holly_zhou@yahoo.com>
2016-09-30 00:20:46 -0700

    changed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
-    publicPath: '/build/'
+    publicPath: 'build/'
   },
   devtool: 'inline-source-map',
   devServer: {
