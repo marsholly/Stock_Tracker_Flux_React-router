33b276e209449934b0f5376bddcdc40e5b969314
HONG ZHOU <holly_zhou@yahoo.com>
2016-09-29 23:40:41 -0700

    added slash to index.html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 
 import App from './components/App';
 import CompanyList from './components/CompanyList';
