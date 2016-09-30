178c43802daaa7eb7430e8f7e5e0c732a2b51d8d
HONG ZHOU <holly_zhou@yahoo.com>
2016-09-29 23:51:49 -0700

    removed ./build & /build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
 import App from './components/App';
 import CompanyList from './components/CompanyList';
@@ -11,7 +11,7 @@ render(
     <Route path = '/' component = {App}>
       <Route path = 'stock' component = {StockView} />
     </Route>
-      <Route path = 'companies' component = {CompanyList}/>
+    <Route path = 'companies' component = {CompanyList}/>
   </Router>,
   document.getElementById('root')
 )
